9b6d3bd84b82b8f4e1524f0f9fd58edd4e741043
fergim1 <fernando@movenow.com.ar>
2021-10-23 14:51:42 +0200

    codigo refactorizado

--- src/App.js
+++ src/App.js
@@ -35,8 +35,8 @@ const breakPointsColors = [
   
 
 export const App = () => {
-  const [parte, setParte] = useState('puntera');
-  const [secciones, setSecciones] = useState( {
+  const [sectionSelected, setSectionSelected] = useState('puntera');
+  const [secciones, setSecciones] = useState({
     puntera: whiteSneaker.puntera,
     lateral: whiteSneaker.lateral,
     cordones: whiteSneaker.cordones,
@@ -48,262 +48,108 @@ export const App = () => {
     trensilla: whiteSneaker.trensilla
   })
 
-  const handlePartes = (elegido) => {
-    setParte( elegido )
-    console.log(elegido)
+  const handleSectionSelected = ( sectionSelected ) => {
+    setSectionSelected( sectionSelected )
   }
 
-  const handleColorPuntera = (color) => {
-    setSecciones( {
-      ...secciones,
-      puntera: color
-    } )
-  }
-  
-  const handleColorLateral = (color) => {
-    setSecciones( {
-      ...secciones,
-      lateral: color
-    } )
-  }
-  const handleColorSuela = ( color ) => {
-    setSecciones( {
-      ...secciones,
-      suela: color
-    } )
+  const handleColorSelected = ( sectionColor ) => {
+    setSecciones({
+        ...secciones,
+        [sectionSelected]: sectionColor
+    })
+}
+
+  const handleColorsBySection = ( array ) => {
+    return (
+          array.map( (obj, index ) => {
+            return (
+                < div className='styling-example' key={index}>
+                    <img 
+                      className='colors-img' 
+                      src= { obj.colorImg } 
+                      alt={ obj.color } 
+                      onClick={ () => handleColorSelected( obj.sectionColor)  }
+                      />  
+              </div>
+            )
+          })
+    )
   }
 
-  const handleColorCordones = ( color ) => {
-    setSecciones( {
-      ...secciones,
-      cordones: color
-    } )
-  }
-
-  const handleColorInterior = ( color ) => {
-    setSecciones( {
-      ...secciones,
-      interior: color
-    } )
-  }
-
-  const handleColorOjalillos = ( color ) => {
-    setSecciones( {
-      ...secciones,
-      ojalillos: color
-    } )
-  }
-
-  const handleColorTrensilla = ( color ) => {
-    setSecciones( {
-      ...secciones,
-      trensilla: color
-    } )
-  }
-
-  const handleColorTalon = ( color ) => {
-    setSecciones( {
-      ...secciones,
-      talon: color
-    } )
-  }
-
-  const handleColorLengueta = ( color ) => {
-    setSecciones( {
-      ...secciones,
-      lengueta: color
-    } )
-  }
 
   return (
     <div className="App">
-      <div className='logo-wrapper'>
-          <img
-            src={ Logo }
-            className='logo-img'
-            alt='Move-Now-logo'
-          />
-      </div>
+        <div className='logo-wrapper'>
+            <img
+              src={ Logo }
+              className='logo-img'
+              alt='Move-Now-logo'
+            />
+        </div>
 
-      <div className='sneaker-wrapper'>
-        <img src= { whiteSneakerBackground } alt='sneaker-background' className='sneaker-img'/>
-        <img src= { secciones.cordones } alt='cordones' className='sneaker-img'/>
-        <img src= { secciones.interior } alt='interior' className='sneaker-img'/>
-        <img src= { secciones.lateral } alt='lateral'  className='sneaker-img'/>
-        <img src= { secciones.lengueta } alt='lengueta'  className='sneaker-img'/>
-        <img src= { secciones.ojalillos } alt='ojalillos'  className='sneaker-img'/>
-        <img src= { secciones.puntera } alt='puntera' className='sneaker-img'/>
-        <img src= { secciones.suela } alt='suela'  className='sneaker-img'/>
-        <img src= { secciones.talon } alt='talon'  className='sneaker-img'/>
-        <img src= { secciones.trensilla } alt='trensilla'  className='sneaker-img'/>
+        <div className='sneaker-wrapper'>
+          <img src= { whiteSneakerBackground } alt='sneaker-background' className='sneaker-img'/>
+          <img src= { secciones.cordones } alt='cordones' className='sneaker-img'/>
+          <img src= { secciones.interior } alt='interior' className='sneaker-img'/>
+          <img src= { secciones.lateral } alt='lateral'  className='sneaker-img'/>
+          <img src= { secciones.lengueta } alt='lengueta'  className='sneaker-img'/>
+          <img src= { secciones.ojalillos } alt='ojalillos'  className='sneaker-img'/>
+          <img src= { secciones.puntera } alt='puntera' className='sneaker-img'/>
+          <img src= { secciones.suela } alt='suela'  className='sneaker-img'/>
+          <img src= { secciones.talon } alt='talon'  className='sneaker-img'/>
+          <img src= { secciones.trensilla } alt='trensilla'  className='sneaker-img'/>
+        </div>
 
-      </div>
-
-      <div className='sections-wrapper'>
-          <Carousel 
-            breakPoints={breakPointsSectios}
-            pagination={false}
+        <div className='sections-wrapper'>
+            <Carousel 
+                breakPoints={breakPointsSectios}
+                pagination={false}
             >
-              {
-                Partes.map((obj, index ) => 
-                  < div className='styling-example' key={index}>
-                      <button 
-                        className='section-wrapper'
-                        onClick= { () => handlePartes ( obj.nombre)}
-                      >
-                            <img 
-                              className='sections-img'
-                              src={obj.seccion} 
-                              alt={ obj.nombre }
-                            />
-                            <p
-                              className={`sections-name ${ parte===obj.nombre && 'selected' } `}
-                            > 
-                              {obj.nombre} 
-                            </p>         
-                      </button>
-                  </div>)
-                }
-          </Carousel>
-      </div>
+                {
+                  Partes.map((obj, index ) => 
+                    < div className='styling-example' key={index}>
+                          <button 
+                            className='section-wrapper'
+                            onClick= { () => handleSectionSelected( obj.nombre)}
+                          >
+                                <img 
+                                  className='sections-img'
+                                  src={obj.seccion} 
+                                  alt={ obj.nombre }
+                                />
+                                <p className={`sections-name ${ sectionSelected===obj.nombre && 'selected' } `}> 
+                                  {obj.nombre} 
+                                </p>         
+                          </button>
+                    </div>)
+                  }
+            </Carousel>
+        </div>
 
-      <div className='colors-wrapper'>
-      <Carousel 
-            breakPoints={breakPointsColors}
-            pagination={false}
-      >              
-          {
-            parte === 'puntera'
-            &&
-            punteras.map((puntera, index ) =>               
-                < div className='styling-example' key={index}>
-                        <img 
-                          className='colors-img' 
-                          src= { puntera.cuadrado } 
-                          alt='' 
-                          onClick= { () => handleColorPuntera ( puntera.parte ) } 
-                        />               
-                </div>
-            )
-          }
-          {
-            parte === 'lateral'
-            &&
-            laterales.map((lateral, index ) =>               
-                < div className='styling-example' key={index}>
-                        <img 
-                          className='colors-img' 
-                          src= { lateral.cuadrado } 
-                          alt='' 
-                          onClick= { () => handleColorLateral ( lateral.parte ) } 
-                        />               
-                </div>
-            )
-          }
-          {
-            parte === 'suela'
-            &&
-            suelas.map((suela, index ) =>               
-                < div className='styling-example' key={index}>
-                        <img 
-                          className='colors-img' 
-                          src= { suela.cuadrado } 
-                          alt='' 
-                          onClick= { () => handleColorSuela ( suela.parte ) } 
-                        />               
-                </div>
-            )
-          }
-          {
-            parte === 'cordones'
-            &&
-            cordones.map((cordon, index ) =>               
-                < div className='styling-example' key={index}>
-                        <img 
-                          className='colors-img' 
-                          src= { cordon.cuadrado } 
-                          alt='' 
-                          onClick= { () => handleColorCordones ( cordon.parte ) } 
-                        />               
-                </div>
-            )
-          }
-          {
-            parte === 'interior'
-            &&
-            interiores.map((interior, index ) =>               
-                < div className='styling-example' key={index}>
-                        <img 
-                          className='colors-img' 
-                          src= { interior.cuadrado } 
-                          alt='' 
-                          onClick= { () => handleColorInterior ( interior.parte ) } 
-                        />               
-                </div>
-            )
-          }
-          {
-            parte === 'ojalillos'
-            &&
-            ojalillos.map((ojalillo, index ) =>               
-                < div className='styling-example' key={index}>
-                        <img 
-                          className='colors-img' 
-                          src= { ojalillo.cuadrado } 
-                          alt='negro' 
-                          onClick= { () => handleColorOjalillos ( ojalillo.parte ) } 
-                        />               
-                </div>
-            )
-          }
-          {
-            parte === 'trensilla'
-            &&
-            trensillas.map((trensilla, index ) =>               
-                < div className='styling-example' key={index}>
-                        <img 
-                          className='colors-img' 
-                          src= { trensilla.cuadrado } 
-                          alt='negro' 
-                          onClick= { () => handleColorTrensilla ( trensilla.parte ) } 
-                        />               
-                </div>
-            )
-          }
-          {
-            parte === 'talon'
-            &&
-            talones.map((talon, index ) =>               
-                < div className='styling-example' key={index}>
-                        <img 
-                          className='colors-img' 
-                          src= { talon.cuadrado } 
-                          alt='negro' 
-                          onClick= { () => handleColorTalon ( talon.parte ) } 
-                        />               
-                </div>
-            )
-          }
-          {
-            parte === 'lengueta'
-            &&
-            lenguetas.map((lengueta, index ) =>               
-                < div className='styling-example' key={index}>
-                        <img 
-                          className='colors-img' 
-                          src= { lengueta.cuadrado } 
-                          alt='negro' 
-                          onClick= { () => handleColorLengueta ( lengueta.parte ) } 
-                        />               
-                </div>
-            )
-          }
-
-      </Carousel>
-
-      </div>
-
-
+        <div className='colors-wrapper'>
+            <Carousel 
+                  breakPoints={breakPointsColors}
+                  pagination={false}
+            >              
+                { sectionSelected === 'puntera' && handleColorsBySection( punteras ) }
+                
+                { sectionSelected === 'lateral' && handleColorsBySection( laterales ) }
+                
+                { sectionSelected === 'suela' && handleColorsBySection( suelas ) }
+                
+                { sectionSelected === 'cordones' && handleColorsBySection( cordones ) }
+                
+                { sectionSelected === 'interior' && handleColorsBySection( interiores ) }
+                
+                { sectionSelected === 'ojalillos' && handleColorsBySection( ojalillos ) }
+                
+                { sectionSelected === 'trensilla' && handleColorsBySection( trensillas ) }
+                
+                { sectionSelected === 'talon' && handleColorsBySection( talones ) }
+                  
+                { sectionSelected === 'lengueta' && handleColorsBySection( lenguetas ) }
+            </Carousel>
+        </div>
     </div>
   );
 }
